1d5ca0edef16e9a210a34593ac4538e4fcd4f8df
アクゼスティア <akzestia@gmail.com>
2025-09-05 20:03:05 +0900

    Update server bin, using new builder implementation

--- zetax-types/src/bin/server.rs
+++ zetax-types/src/bin/server.rs
@@ -18,9 +18,12 @@ fn main() -> Result<()> {
     register_rpc(&RPC_ECHO);
     register_rpc(&RPC_HELLO);
 
-    let mut settings = ServerSettings::default();
-    settings.listen = "0.0.0.0:4433".parse().unwrap();
-    settings.workers = num_cpus::get();
+    let settings = ServerSettings::builder()
+        .workers(num_cpus::get())
+        .listen("0.0.0.0:4433".parse().unwrap())
+        .cert_file("cert.crt")
+        .key_file("cert.key")
+        .build();
 
     run_server(settings)
 }
